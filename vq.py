ad5356349c46d6e6e0774b9e7cb2a985e84ec0de
taka <d@wakou.cc>
2025-03-02 11:23:00 +0900

    chunk 500 from 1000

--- vq.py
+++ vq.py
@@ -396,7 +396,7 @@ def batched_embedding(indices, embeds):
     return embeds.gather(2, indices)
 
 
-def compute_contrastive_loss(z, atom_types, margin=1.0, threshold=0.5, num_atom_types=100):
+def compute_contrastive_loss(z, atom_types, margin=10.0, threshold=0.95, num_atom_types=100):
     """
     Contrastive loss to separate different atom types using embeddings.
     """
