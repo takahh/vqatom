18af7ac1df4400b9301f22a3bf7508f4326ed487
taka <d@wakou.cc>
2025-03-02 10:39:02 +0900

    no more extended_g because no explicit hop edges

--- new_train_and_eval.py
+++ new_train_and_eval.py
@@ -206,6 +206,7 @@ def convert_to_dgl(adj_batch, attr_batch, device="cuda"):
             base_g.ndata["feat"] = filtered_attr_matrix
             base_g.edata["weight"] = edge_weights.float()
             base_g.edata["edge_type"] = torch.ones(base_g.num_edges(), dtype=torch.int, device=device)
+            base_g = dgl.add_self_loop(base_g)
             base_graphs.append(base_g)
             #
             # # ------------------------------------------
