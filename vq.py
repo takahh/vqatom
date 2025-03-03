4974e7254ee267f281620b1bcb42bdaa889ad5c5
taka <d@wakou.cc>
2025-03-03 20:05:16 +0900

    constrastive loss exp introduces.

--- vq.py
+++ vq.py
@@ -1221,7 +1221,7 @@ class VectorQuantize(nn.Module):
         ringy_div_loss = compute_contrastive_loss(quantized, init_feat[:, 5], "ringy")
         h_num_div_loss = compute_contrastive_loss(quantized, init_feat[:, 6], "h_num")
 
-        atom_type_div_loss = cluster_penalty_loss(init_feat, quantized)
+        atom_type_div_loss = cluster_penalty_loss(init_feat, embed_ind_for_sil)
 
         return (margin_loss, spread_loss, pair_distance_loss, atom_type_div_loss, bond_num_div_loss, aroma_div_loss,
                 ringy_div_loss, h_num_div_loss, sil_loss, embed_ind, charge_div_loss, elec_state_div_loss, equivalent_atom_loss)
