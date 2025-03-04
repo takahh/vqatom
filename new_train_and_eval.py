9e3ea7a4336185ab12c7dd5b8ba8cc901c59f0fc
taka <d@wakou.cc>
2025-03-04 22:10:10 +0900

    constrastive loss exp introduces.

--- new_train_and_eval.py
+++ new_train_and_eval.py
@@ -72,7 +72,7 @@ def train_sage(model, g, feats, optimizer, epoch, logger):
     scaler = torch.cuda.amp.GradScaler()
     optimizer.zero_grad()
 
-    with torch.cuda.amp.autocast():
+    with torch.cuda.amp.autocast(dtype=torch.float16):
         _, logits, loss, _, cb, loss_list3, latent_train, quantized, latents, sample_list_train = model(g, feats, epoch,
                                                                                                         logger)  # g is blocks
     loss = loss.to(device)
