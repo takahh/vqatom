5e8f386bf439be21fe6dd64ee565ec816ebf5b16
taka <d@wakou.cc>
2025-03-06 06:51:50 +0900

    constrastive loss exp introduces.

--- vq.py
+++ vq.py
@@ -473,7 +473,7 @@ def cluster_penalty_loss(feats, quantized, cluster_assignments): # init_feat, qu
     # Gaussian-based penalty function (or alternative)
     diff_feat_same_cluster_dist = dist_matrix * (diff_feat_mask * same_id_mask).detach()
     non_zero_values = diff_feat_same_cluster_dist[diff_feat_same_cluster_dist != 0]
-    penalty = torch.logsumexp(-diff_feat_same_cluster_dist, dim=-1).mean()*100000
+    penalty = torch.logsumexp(-diff_feat_same_cluster_dist, dim=-1).mean()*100000000000000
     return penalty
 
 
