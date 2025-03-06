36cb537b9815ab53d418c028fa5bbcb9dbebc3c0
taka <d@wakou.cc>
2025-03-06 16:17:10 +0900

    soft_kmeans debug

--- train_teacher.py
+++ train_teacher.py
@@ -126,7 +126,7 @@ def get_args():
     )
 
     """SAGE Specific"""
-    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--batch_size", type=int, default=10)
     parser.add_argument(
         "--fan_out",
         type=str,
@@ -138,7 +138,7 @@ def get_args():
     )
 
     parser.add_argument(
-        "--chunk_size", type=int, default=50
+        "--chunk_size", type=int, default=5
     )
 
     """Optimization"""
