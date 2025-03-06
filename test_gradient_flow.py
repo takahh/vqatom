019ff2f68ca46c4753cf6fd741df83835503b942
taka <d@wakou.cc>
2025-03-06 17:56:52 +0900

    soft_kmeans debug

--- test_gradient_flow.py
+++ test_gradient_flow.py
@@ -3,7 +3,7 @@ from vq import VectorQuantize
 
 x = torch.randn(10, 64, device="cuda", requires_grad=True)  # Example input
 init_feat = torch.randn(10, 64, device="cuda", requires_grad=True)  # Example feature
-vq = VectorQuantize(dim=64, codebook_size=100).cuda()  # Instantiate the module
+vq = VectorQuantize(dim=64, codebook_size=1000).cuda()  # Instantiate the module
 quantize, _, loss, *_ = vq(x, init_feat, logger=None)
 
 # Compute loss and backpropagate
