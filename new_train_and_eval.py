c61474dc28e0ffb96fb9e0a0e8d1ce562f9fdf54
taka <d@wakou.cc>
2025-03-08 07:38:29 +0900

    vq codebook lr to 0.0001

--- new_train_and_eval.py
+++ new_train_and_eval.py
@@ -161,7 +161,8 @@ def evaluate(model, g, feats, epoch, logger, g_base):
     loss_list, latent_list, cb_list, loss_list_list = [], [], [], []
     # with torch.no_grad(), autocast():
     with torch.no_grad():
-        _, logits, test_loss, _, cb, test_loss_list3, latent_train, quantized, test_latents, sample_list_test = model(g, feats, epoch, logger, g_base)  # g is blocks
+        #   logits, loss, _, cb, loss_list3, latent_train, quantized, latents, sample_list_train = model(g, feats, epoch, logger)
+        logits, test_loss, _, cb, test_loss_list3, latent_train, quantized, test_latents, sample_list_test = model(g, feats, epoch, logger, g_base)  # g is blocks
     latent_list.append(latent_train.detach().cpu())
     cb_list.append(cb.detach().cpu())
     test_latents = test_latents.detach().cpu()
