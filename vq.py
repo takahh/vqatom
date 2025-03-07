6d221dfd4a7cd871d26e0e0ad41b0e8e0c850c02
taka <d@wakou.cc>
2025-03-07 15:47:50 +0900

    loss / 100

--- vq.py
+++ vq.py
@@ -572,9 +572,9 @@ def compute_contrastive_loss(z, atom_types, name, margin=0.2, threshold=0.5, num
         print("Error in one_hot:", e)
         print("Atom types values:", atom_types)
         raise
-    print(f"pairwise_distances.max() {pairwise_distances.max()}")
     # Compute pairwise distances for the z vectors
     pairwise_distances = torch.cdist(z, z, p=2)
+    print(f"pairwise_distances.max() {pairwise_distances.max()}")
     pairwise_distances = pairwise_distances / (pairwise_distances.max() + 1e-6)  # Normalize to [0,1]
 
     # Normalize the atom_types vectors
