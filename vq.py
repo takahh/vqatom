90fa7f1a7d026f8d1acfe47906bed35239cfe5b0
taka <d@wakou.cc>
2025-03-10 00:39:04 +0900

    bond weight updated

--- vq.py
+++ vq.py
@@ -569,7 +569,7 @@ def cluster_penalty_loss(feats, quantized, cluster_assignments): # init_feat, qu
     # penalty = 10 * torch.logsumexp(-diff_feat_same_cluster_dist / 10, dim=-1).mean()
     penalty = (diff_feat_same_cluster_dist + 1e-6).log().mean()/1000000
 
-    # print(f"penalty {penalty}")
+    print(f"penalty {penalty}")
     return penalty
 
 
