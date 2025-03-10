955bbd093914b48b05b30664d57b390739b6aa00
taka <d@wakou.cc>
2025-03-11 08:22:26 +0900

    compute_constrastive loss, atomtypes is now vectors

--- 0227version/new_train_and_eval.py
+++ 0227version/new_train_and_eval.py
@@ -322,8 +322,6 @@ def run_inductive(
                     # Ensure node features are correctly extracted
                     with torch.no_grad():
                         batched_feats = batched_graph.ndata["feat"]
-                    print("batched_feats")
-                    print(batched_feats.shape)
                     # batched_feats = batched_graph.ndata["feat"]
                     loss, loss_list_train, latent_train, latents = train_sage(
                         model, batched_graph, batched_feats, optimizer, epoch, logger)
