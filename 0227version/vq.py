2ac320bb49ab8acc234ec4e07fb4d6a1a3a1a0ad
taka <d@wakou.cc>
2025-03-15 07:31:48 +0900

    embed_ind to discrete with ste

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -502,7 +502,7 @@ def compute_contrastive_loss(z, atom_types, threshold=0.5, num_atom_types=20):
     # Create mask for "same type"
     same_type_mask = (pairwise_similarities >= 1).float()
     close_type_mask_0 = (1 > pairwise_similarities).float()
-    close_type_mask_1 = (pairwise_similarities > 0.8).float()
+    close_type_mask_1 = (pairwise_similarities > 0.9).float()
     print("pairwise_similarities")
     print(pairwise_similarities)
     # Compute positive and negative losses
