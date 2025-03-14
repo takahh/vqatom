90bc31427827cb43108b36b811c4b79bccab1eee
taka <d@wakou.cc>
2025-03-15 07:35:55 +0900

    embed_ind to discrete with ste

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -492,25 +492,20 @@ def compute_contrastive_loss(z, atom_types, threshold=0.5, num_atom_types=20):
     # Compute pairwise distances for the z vectors
     pairwise_distances = torch.cdist(z, z, p=2)
     pairwise_distances = pairwise_distances / (pairwise_distances.max() + 1e-6)  # Normalize to [0,1]
-    print("pairwise_distances.mean()")
-    print(pairwise_distances.mean())
-    print("pairwise_distances.min()")
-    print(pairwise_distances.min())
+    close_dist_mask = (0.2 > pairwise_distances).float()
+
     # Normalize atom_types (now properly converted to float)
     atom_types = F.normalize(atom_types, p=2, dim=1)
-
     # Compute pairwise similarity for the atom_types
     pairwise_similarities = torch.mm(atom_types, atom_types.T)  # Cosine similarity
-
     # Create mask for "same type"
     same_type_mask = (pairwise_similarities >= 1).float()
     close_type_mask_0 = (1 > pairwise_similarities).float()
     close_type_mask_1 = (pairwise_similarities > 0.9).float()
-    print("pairwise_similarities")
-    print(pairwise_similarities)
+
     # Compute positive and negative losses
     positive_loss = same_type_mask * pairwise_distances ** 2
-    negative_loss = close_type_mask_0 * close_type_mask_1 * pairwise_distances
+    negative_loss = close_type_mask_0 * close_type_mask_1 * (pairwise_distances * close_dist_mask)
     negative_loss = - torch.log(negative_loss + 1e-8)
     return negative_loss.mean()/100
     # return (positive_loss.mean() + negative_loss.mean()/100)
