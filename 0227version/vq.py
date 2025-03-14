4ed575d4a6a04471a3690f7a7ca6e8873e92924a
taka <d@wakou.cc>
2025-03-15 07:01:28 +0900

    embed_ind to discrete with ste

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -505,7 +505,7 @@ def compute_contrastive_loss(z, atom_types, threshold=0.5, num_atom_types=20):
     # Compute positive and negative losses
     positive_loss = same_type_mask * pairwise_distances ** 2
     negative_loss = (1.0 - same_type_mask) * pairwise_distances ** 2
-    negative_loss = torch.exp(- negative_loss.mean())
+    negative_loss = - torch.log(negative_loss)
     return (positive_loss.mean() + negative_loss.mean())
 
 
