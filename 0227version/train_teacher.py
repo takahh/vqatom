2504e94157871546234b08e5cc157b926b0c7ed8
taka <d@wakou.cc>
2025-03-19 04:28:48 +0900

    egnn

--- 0227version/train_teacher.py
+++ 0227version/train_teacher.py
@@ -141,7 +141,7 @@ def get_args():
     )
 
     parser.add_argument(
-        "--chunk_size", type=int, default=2000
+        "--chunk_size", type=int, default=1000
     )
 
     """Optimization"""
