935f07b9775a3f38a1d5c2d96f8c408543223c42
taka <d@wakou.cc>
2025-03-19 13:06:41 +0900

    egnn

--- 0227version/models.py
+++ 0227version/models.py
@@ -145,8 +145,8 @@ class EquivariantThreeHopGINE(nn.Module):
 
         self.bond_weight = self.bond_weight.to(device)
         data = data.to(device)
-        features = transform_node_feats(features).to(device)  # Ensure this function is defined
-        # features = features.to(device)  # Ensure this function is defined
+        # features = transform_node_feats(features).to(device)  # Ensure this function is defined
+        features = features.to(device)  # Ensure this function is defined
         # Initial node feature transformation
         h = self.linear_0(features)
 
