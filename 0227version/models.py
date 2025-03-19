177454d677df2bc2d34799de4c615f8deac0c001
taka <d@wakou.cc>
2025-03-19 11:54:24 +0900

    egnn

--- 0227version/models.py
+++ 0227version/models.py
@@ -210,8 +210,8 @@ class EquivariantThreeHopGINE(nn.Module):
 
         if batched_graph_base:
             # sample_adj_base = batched_graph_base.adj(sparse_fmt="coo").to_dense()
-            sample_adj_base = batched_graph_base.detach().adj().to_dense()
-            sample_bond_info = batched_graph_base.detach().edata["weight"]
+            sample_adj_base = batched_graph_base.adj().to_dense()
+            sample_bond_info = batched_graph_base.edata["weight"]
             sample_list = [emb_ind, features, sample_adj, sample_bond_info, src, dst, sample_adj_base]
         else:
             sample_bond_info = data.edata["weight"]
