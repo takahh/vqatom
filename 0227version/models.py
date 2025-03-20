26f5533d9af993a42fc7d4703bf2cb280ecdf207
taka <d@wakou.cc>
2025-03-20 12:26:41 +0900

    egnn

--- 0227version/models.py
+++ 0227version/models.py
@@ -174,7 +174,7 @@ class EquivariantThreeHopGINE(nn.Module):
         # Extract edge indices and construct edge_index tensor
         src, dst = data.edges()
         edge_index = torch.stack([src, dst], dim=0)  #　隣接情報
-        edge_attr = torch.ones((transformed_edge_weight.shape[0], transformed_edge_weight.shape[1]), device=src.device) # 結合情報　全部１
+        edge_attr = torch.ones((transformed_edge_weight[0], transformed_edge_weight[1]), device=src.device) # 結合情報　全部１
         print(f"h {h.shape}")
         print(f"edge_index {edge_index.shape}")
         print(f"edge_index {edge_index}")
