3a87c38ecb3dc1cdbc316c6b741358097bc05fb7
taka <d@wakou.cc>
2025-03-20 11:30:55 +0900

    egnn

--- 0227version/models.py
+++ 0227version/models.py
@@ -174,19 +174,19 @@ class EquivariantThreeHopGINE(nn.Module):
         # GINE Layer 1
         h = self.gine1(h, edge_index=edge_index, edge_attr=edge_attr)
         # h = self.gine1(h, edge_index=edge_index)
-        h = self.ln0(h)
+        # h = self.ln0(h)
         # h = self.leaky_relu0(h)
 
         # GINE Layer 2
         h = self.gine2(h, edge_index=edge_index, edge_attr=edge_attr)
         # h = self.gine2(h, edge_index=edge_index)
-        h = self.ln1(h)
+        # h = self.ln1(h)
         # h = self.leaky_relu1(h)
 
         # GINE Layer 3
         h = self.gine3(h, edge_index=edge_index, edge_attr=edge_attr)
         # h = self.gine3(h, edge_index=edge_index)
-        h = self.ln2(h)
+        # h = self.ln2(h)
 
         # Vector Quantization
         init_feat = features.clone()
