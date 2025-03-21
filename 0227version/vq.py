f8efe6d70fecdb14a1e96f6f298fb3bb7d6021ca
taka <d@wakou.cc>
2025-03-21 19:34:44 +0900

    egnn

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -1098,7 +1098,6 @@ class VectorQuantize(nn.Module):
             threshold_ema_dead_code=0,
             channel_last=True,
             accept_image_fmap=False,
-            commitment_weight=0.1,
             margin_weight=1,
             spread_weight=0.2,
             pair_weight=0.01,
@@ -1110,8 +1109,9 @@ class VectorQuantize(nn.Module):
             lamb_div_equidist=1,
             lamb_div_elec_state=1,
             lamb_div_charge=1,
-            lamb_sil=0.1,
-            lamb_div=100,
+            commitment_weight=0.001,  # using
+            lamb_sil=0.001,           # using
+            lamb_div=1,           # using
             lamb_equiv_atom=1,
             orthogonal_reg_active_codes_only=False,
             orthogonal_reg_max_codes=None,
