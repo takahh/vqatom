c54bef7e8ca6989315b871188510360c69147213
taka <d@wakou.cc>
2025-03-23 10:42:27 +0900

    data size adjusted

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -502,6 +502,7 @@ def compute_contrastive_loss(z, atom_types, threshold=0.5, num_atom_types=20):
     negative_loss = close_type_mask_0 * close_type_mask_1 * (pairwise_distances * close_dist_mask)
     # negative_loss = - torch.log(negative_loss + 1e-8)
     negative_loss = torch.pow(0.1 - negative_loss, 2)
+    print(f"negative_loss min: {negative_loss.min()}, mean {negative_loss.mean()}, max {negative_loss.max()}")
     return negative_loss.mean()
     # return (positive_loss.mean() + negative_loss.mean()/100)
 
