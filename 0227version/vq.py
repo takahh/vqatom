9d5539f3f73ef824ad2a507e860f3b4bf36faa31
taka <d@wakou.cc>
2025-03-30 15:35:30 +0900

    transform_node_feats restored

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -1004,7 +1004,7 @@ class VectorQuantize(nn.Module):
             lamb_div_elec_state=1,
             lamb_div_charge=1,
             commitment_weight=0.01,  # using
-            lamb_sil=1,           # using
+            lamb_sil=0.00001,           # using
             lamb_cb=0.01,           # using
             lamb_div=0.0001,           # using
             lamb_equiv_atom=1,
