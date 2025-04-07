5b199ae192655266a35b0606881fb131f23f1ff2
taka <d@wakou.cc>
2025-04-08 06:33:58 +0900

    GPU usage is low so edited

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -549,7 +549,7 @@ def compute_contrastive_loss(z, atom_types, margin=1.0, temperature=0.1):
 
     # Final loss combining contrastive and orthogonality components
     final_loss = loss + 0.0001 * orthogonality_reg
-    print(f"loss {loss}, orthogonality reg {orthogonality_reg * 0.0001}")
+    # print(f"loss {loss}, orthogonality reg {orthogonality_reg * 0.0001}")
 
     return final_loss
 
