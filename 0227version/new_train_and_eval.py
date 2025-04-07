8ed315c4e9f3e1bf967e40bbef59cbd907bbb98c
taka <d@wakou.cc>
2025-04-07 16:03:12 +0900

    GPU usage is low so edited

--- 0227version/new_train_and_eval.py
+++ 0227version/new_train_and_eval.py
@@ -401,38 +401,38 @@ def run_inductive(
                 gc.collect()
                 torch.cuda.empty_cache()
                 loss_list_list_test = [x + [y] for x, y in zip(loss_list_list_test, loss_list_test)]
+        if conf['train_or_infer'] == "train":
+            print(f"epoch {epoch}: loss {sum(loss_list)/len(loss_list):.9f}, test_loss {sum(test_loss_list)/len(test_loss_list):.9f}")
+            logger.info(f"epoch {epoch}: loss {sum(loss_list)/len(loss_list):.9f}, test_loss {sum(test_loss_list)/len(test_loss_list):.9f}")
+            print(
+                f"train - feat_div loss: {sum(loss_list_list_train[0]) / len(loss_list_list_train[0]): 9f}, "
+                f"train - commit_loss: {sum(loss_list_list_train[1]) / len(loss_list_list_train[1]): 9f}, "
+                f"train - cb_loss: {sum(loss_list_list_train[2]) / len(loss_list_list_train[2]): 9f},"
+                f"train - sil_loss: {sum(loss_list_list_train[3]) / len(loss_list_list_train[3]): 9f},"
+            )
 
-        print(f"epoch {epoch}: loss {sum(loss_list)/len(loss_list):.9f}, test_loss {sum(test_loss_list)/len(test_loss_list):.9f}")
-        logger.info(f"epoch {epoch}: loss {sum(loss_list)/len(loss_list):.9f}, test_loss {sum(test_loss_list)/len(test_loss_list):.9f}")
-        print(
-            f"train - feat_div loss: {sum(loss_list_list_train[0]) / len(loss_list_list_train[0]): 9f}, "
-            f"train - commit_loss: {sum(loss_list_list_train[1]) / len(loss_list_list_train[1]): 9f}, "
-            f"train - cb_loss: {sum(loss_list_list_train[2]) / len(loss_list_list_train[2]): 9f},"
-            f"train - sil_loss: {sum(loss_list_list_train[3]) / len(loss_list_list_train[3]): 9f},"
-        )
+            print(
+                  f"train - feat_div loss: {sum(loss_list_list_test[0]) / len(loss_list_list_test[0]): 9f}, "
+                  f"train - commit_loss: {sum(loss_list_list_test[1]) / len(loss_list_list_test[1]): 9f}, "
+                  f"train - cb_loss: {sum(loss_list_list_test[2]) / len(loss_list_list_test[2]): 9f},"
+                  f"train - sil_loss: {sum(loss_list_list_test[3]) / len(loss_list_list_test[3]): 9f},"
+                  )
 
-        print(
-              f"train - feat_div loss: {sum(loss_list_list_test[0]) / len(loss_list_list_test[0]): 9f}, "
-              f"train - commit_loss: {sum(loss_list_list_test[1]) / len(loss_list_list_test[1]): 9f}, "
-              f"train - cb_loss: {sum(loss_list_list_test[2]) / len(loss_list_list_test[2]): 9f},"
-              f"train - sil_loss: {sum(loss_list_list_test[3]) / len(loss_list_list_test[3]): 9f},"
-              )
+            # Log training losses
+            logger.info(
+                f"train - feat_div loss: {sum(loss_list_list_train[0]) / len(loss_list_list_train[0]): 9f}, "
+                f"train - commit_loss: {sum(loss_list_list_train[1]) / len(loss_list_list_train[1]): 9f}, "
+                f"train - cb_loss: {sum(loss_list_list_train[2]) / len(loss_list_list_train[2]): 9f},"
+                f"train - sil_loss: {sum(loss_list_list_train[3]) / len(loss_list_list_train[3]): 9f},"
+            )
 
-        # Log training losses
-        logger.info(
-            f"train - feat_div loss: {sum(loss_list_list_train[0]) / len(loss_list_list_train[0]): 9f}, "
-            f"train - commit_loss: {sum(loss_list_list_train[1]) / len(loss_list_list_train[1]): 9f}, "
-            f"train - cb_loss: {sum(loss_list_list_train[2]) / len(loss_list_list_train[2]): 9f},"
-            f"train - sil_loss: {sum(loss_list_list_train[3]) / len(loss_list_list_train[3]): 9f},"
-        )
-
-        # Log testing losses
-        logger.info(
-            f"train - feat_div loss: {sum(loss_list_list_test[0]) / len(loss_list_list_test[0]): 9f}, "
-              f"train - commit_loss: {sum(loss_list_list_test[1]) / len(loss_list_list_test[1]): 9f}, "
-              f"train - cb_loss: {sum(loss_list_list_test[2]) / len(loss_list_list_test[2]): 9f},"
-              f"train - sil_loss: {sum(loss_list_list_test[3]) / len(loss_list_list_test[3]): 9f},"
-        )
+            # Log testing losses
+            logger.info(
+                f"train - feat_div loss: {sum(loss_list_list_test[0]) / len(loss_list_list_test[0]): 9f}, "
+                  f"train - commit_loss: {sum(loss_list_list_test[1]) / len(loss_list_list_test[1]): 9f}, "
+                  f"train - cb_loss: {sum(loss_list_list_test[2]) / len(loss_list_list_test[2]): 9f},"
+                  f"train - sil_loss: {sum(loss_list_list_test[3]) / len(loss_list_list_test[3]): 9f},"
+            )
         if conf['train_or_infer'] == "infer":
             np.savez(f"./sample_emb_ind_{epoch}", sample_list_test[0].cpu())
             np.savez(f"./sample_node_feat_{epoch}", sample_list_test[1].cpu())
