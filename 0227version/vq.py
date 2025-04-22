54f9b9f664ea8fb02cd946d63093ec2111684829
taka <d@wakou.cc>
2025-04-23 08:23:49 +0900

    GPU usage is low so edited

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -554,8 +554,8 @@ class ContrastiveLoss(nn.Module):
         pos_loss = torch.mean((1 - similarity_matrix) * type_mask)
         # neg_loss = torch.mean(F.relu(similarity_matrix - self.margin) * (1 - type_mask))
         neg_loss = torch.mean(F.relu(similarity_matrix - 0.9) * (1 - type_mask))
-        # loss = pos_loss + neg_loss
-        loss = neg_loss
+        loss = pos_loss + neg_loss
+        # loss = neg_loss
         print(f"nega loss: {neg_loss}, pos loss: {pos_loss}")
         # Optional orthogonality regularization
         orthogonality_reg = torch.trace(torch.mm(z.T, z) -
