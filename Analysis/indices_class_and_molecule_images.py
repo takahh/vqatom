5b7affe7cae41b051c1b9bbedb588f03d54a7c31
taka <d@wakou.cc>
2025-04-26 10:35:47 +0900

    GPU usage is low so edited

--- Analysis/indices_class_and_molecule_images.py
+++ Analysis/indices_class_and_molecule_images.py
@@ -24,7 +24,7 @@ CANVAS_WIDTH = 3300
 CANVAS_HEIGHT = 2500
 FONTSIZE = 30
 EPOCH = 1
-PATH = "/Users/taka/Downloads/3000_512 4/"
+PATH = "/Users/taka/Downloads/3000_512 5/"
 
 
 def getdata(filename):
--- Analysis/plot_latents.py
+++ Analysis/plot_latents.py
@@ -5,7 +5,7 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.inf)
 
-path = "//Users/taka/Documents/vqatom_results/3000_512/"
+path = "//Users/taka/Downloads/0227version 4/"
 # path = "/Users/taka/Downloads/"
 # LMIN = 2.5
 # LMAX = 7.5
@@ -141,7 +141,7 @@ def main():
     arr_list = []
     DIMENSION = 512
     BATCH = 8000
-    EPOCH = 77
+    EPOCH = 80
     EPOCH2 = EPOCH + 1
 
     # MODE = "tsne"
