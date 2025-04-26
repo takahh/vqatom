ccef30f865fbf33c688cff304fbc64215d3e156a
taka <d@wakou.cc>
2025-04-27 00:13:52 +0900

    GPU usage is low so edited

--- Analysis/indices_class_and_molecule_images.py
+++ Analysis/indices_class_and_molecule_images.py
@@ -24,7 +24,7 @@ CANVAS_WIDTH = 3300
 CANVAS_HEIGHT = 2500
 FONTSIZE = 30
 EPOCH = 1
-PATH = "/Users/taka/Downloads/1000_128/"
+PATH = "/Users/taka/Downloads/1000_128 2/"
 
 
 def getdata(filename):
