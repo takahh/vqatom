f21fca99b7d6ad9ee6a36107255c58c23efa784c
taka <d@wakou.cc>
2025-05-01 01:07:27 +0900

    success in separate ID for different element

--- Analysis/indices_class_and_molecule_images.py
+++ Analysis/indices_class_and_molecule_images.py
@@ -24,7 +24,7 @@ CANVAS_WIDTH = 3300
 CANVAS_HEIGHT = 2500
 FONTSIZE = 30
 EPOCH = 1
-PATH = "/Users/taka/Downloads/3000_512 17/"
+PATH = "/Users/taka/Downloads/3000_512 18/"
 
 
 def getdata(filename):
