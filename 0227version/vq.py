f072818c936d3bde683e4da10a37f0d5afba722e
taka <d@wakou.cc>
2025-05-01 13:49:11 +0900

    success in separate ID for different element

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -571,10 +571,10 @@ class ContrastiveLoss(nn.Module):
         # orthogonality_reg = torch.trace(torch.mm(z.T, z) -
         #                                 torch.eye(z.shape[1], device=z.device)) / z.shape[1]
         print(f"loss {loss}, repel loss: {repel_loss}")
-        if epoch < 5:
-            final_loss = loss + 0.00001 * repel_loss
+        if epoch < 3:
+            final_loss = repel_loss
         else:
-            final_loss = loss
+            final_loss = loss + 0.00001 * repel_loss
         # final_loss = loss + 0.0001 * orthogonality_reg
 
         return final_loss
