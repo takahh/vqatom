12bf9473e1cefd187b3d62e87758a68ea00ff97c
taka <d@wakou.cc>
2025-05-09 11:52:35 +0900

    Success finally

--- Analysis/indices_class_and_molecule_images.py
+++ Analysis/indices_class_and_molecule_images.py
@@ -24,7 +24,7 @@ CANVAS_WIDTH = 2000
 CANVAS_HEIGHT = 1500
 FONTSIZE = 20
 EPOCH = 1
-PATH = "/Users/taka/Downloads/10000_128 6/"
+PATH = "/Users/taka/Downloads/15000_128/"
 
 
 def getdata(filename):
