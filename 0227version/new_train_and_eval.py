2e5b3c9c6fecc76e78423d9dac05933969293819
taka <d@wakou.cc>
2025-06-04 15:34:42 +0900

    分子表示完全成功。15000_128

--- 0227version/new_train_and_eval.py
+++ 0227version/new_train_and_eval.py
@@ -442,6 +442,7 @@ def run_inductive(
             ind_list = [item for sublist in ind_list for item in sublist]
         flat = [item for sublist in ind_list for item in sublist]
         count = Counter(flat)
+        flat = torch.tensor(flat, device=device)  # or just torch.tensor(flat)
         unique_sorted_indices = torch.unique(flat, sorted=True)
         used_cb_vectors_all_epochs = model.vq._codebook.embed[unique_sorted_indices]
 
