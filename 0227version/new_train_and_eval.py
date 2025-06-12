3aaf051908871541b6e6c6daabf9d71271828e29
taka <d@wakou.cc>
2025-06-12 21:51:47 +0900

    分子表示完全成功。15000_128

--- 0227version/new_train_and_eval.py
+++ 0227version/new_train_and_eval.py
@@ -435,17 +435,10 @@ def run_inductive(
         # Count Codebook Frequency and used Codebook
         # -------------------------------------------
         args = get_args()
-        print("ind list 0")
-        print(ind_list)
         if args.train_or_infer == 'use_nonredun_cb_infer':
             ind_list = [item for sublist in ind_list for item in sublist]
             ind_list = [item for sublist in ind_list for item in sublist]
-        # if conf['train_or_infer'] == 'train':
-        #     ind_list = [item for sublist in ind_list for item in sublist]
-        print("ind list 1")
-        print(ind_list)
         flat = [item for sublist in ind_list for item in sublist]
-        print(flat)
         count = Counter(flat)
         import json
         flat = torch.tensor(flat)  # or torch.tensor(flat, dtype=torch.long)
