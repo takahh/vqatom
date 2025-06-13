308e1acd73fdde1933d2b2718d486f71ed58fca5
taka <d@wakou.cc>
2025-06-14 04:14:41 +0900

    分子表示完全成功。15000_128

--- Analysis/plot_latents.py
+++ Analysis/plot_latents.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(threshold=np.inf)
 
-DATA_PATH = "/Users/taka/Documents/final_infer_on_all_data/40000_16/"
-# DATA_PATH = "/Users/taka/Documents/vqatom_train_output/bothloss_40000_16/"
+# DATA_PATH = "/Users/taka/Documents/final_infer_on_all_data/40000_16/"
+DATA_PATH = "/Users/taka/Downloads/"
 DIMENSION = 16
 # BATCH_SIZE = 8000
-EPOCH_START = 1
-SAMPLE_LATENT = 3000000
+EPOCH_START = 3
+SAMPLE_LATENT = 30000
 EPOCH_END = EPOCH_START + 1
-# MODE = "umap"  # Choose between "tsne" and "umap"
-MODE = "tsne"  # Choose between "tsne" and "umap"
+MODE = "umap"  # Choose between "tsne" and "umap"
+# MODE = "tsne"  # Choose between "tsne" and "umap"
 
 def load_npz_array(filename):
     """Load and return the array from a .npz file."""
@@ -74,7 +74,7 @@ def plot_tsne(cb_arr, latent_arr, epoch, perplexity, cb_size):
             plt.colorbar(label='Density')
             plt.show()
 
-def plot_umap(cb_arr, latent_arr, epoch, n_neighbors, min_dist, cb_size):
+def plot_umap(cb_arr, latent_arr, epoch, n_neighbors, min_dist, cb_size, zoom):
     print("reducer setup")
     reducer = umap.UMAP(
         n_neighbors=n_neighbors,
@@ -88,8 +88,8 @@ def plot_umap(cb_arr, latent_arr, epoch, n_neighbors, min_dist, cb_size):
     print("latent transform done")
     cb_emb = reducer.transform(cb_arr)
     print("cb transform done")
-    x_range = np.percentile(cb_emb[:, 0], [2, 98])
-    y_range = np.percentile(cb_emb[:, 1], [2, 98])
+    x_range = np.percentile(cb_emb[:, 0], [50 - zoom, 50 + zoom])
+    y_range = np.percentile(cb_emb[:, 1], [50 - zoom, 50 + zoom])
 
     latent_mask = (
         (latent_emb[:, 0] >= x_range[0]) & (latent_emb[:, 0] <= x_range[1]) &
@@ -104,7 +104,7 @@ def plot_umap(cb_arr, latent_arr, epoch, n_neighbors, min_dist, cb_size):
     zoomed_cb = cb_emb[cb_mask]
 
     bins = 200
-    title = f"UMAP: n_neighbors {n_neighbors}, min_dist {min_dist}, epoch {epoch}, cb {cb_size}, dim {latent_arr.shape[-1]}"
+    title = f"UMAP: n_neighbors {n_neighbors}, min_dist {min_dist}, zoom {zoom}"
 
     for i in range(2):
         plt.figure()
@@ -127,14 +127,17 @@ def process_epoch(epoch):
     """Load data and plot visualization for a single epoch."""
     codebook_file = f"{DATA_PATH}used_cb_vectors.npz"
     # codebook_file = "/Users/taka/PycharmProjects/vqatom/Analysis/kmeans_centers.npy"
-    # codebook_file = '/Users/taka/Documents/best_codebook_40000_16/init_codebook_1.npz'
-    latent_file = f"{DATA_PATH}latents_all_{epoch}.npz"
+    codebook_file = f'{DATA_PATH}init_codebook_{epoch}.npz'
+    # latent_file = f"{DATA_PATH}latents_all_{epoch}.npz"
+    latent_file = f"{DATA_PATH}latents_{epoch}.npz"
 
     cb_arr = load_npz_array(codebook_file)
     latent_arr = load_npz_array_multi(latent_file)
     print("latent_arr.shape")
     print(latent_arr.shape)
     latent_arr = latent_arr[:SAMPLE_LATENT]
+    print("latent_arr.shape")
+    print(latent_arr.shape)
     print(cb_arr.shape)
 
     cb_arr = np.unique(cb_arr, axis=0).reshape(-1, DIMENSION)
@@ -143,7 +146,8 @@ def process_epoch(epoch):
     if MODE == "tsne":
         plot_tsne(cb_arr, latent_arr, epoch, perplexity=10, cb_size=cb_size)
     elif MODE == "umap":
-        plot_umap(cb_arr, latent_arr, epoch, n_neighbors=10, min_dist=1.0, cb_size=cb_size)
+        for zoom in [50, 20, 15, 10, 7, 5, 3, 2]:
+            plot_umap(cb_arr, latent_arr, epoch, n_neighbors=10, min_dist=1.0, cb_size=cb_size, zoom=zoom)
 
 
 def main():
