8c366cd3eb84eb03843417e902e6da6367019608
taka <d@wakou.cc>
2025-06-17 00:30:21 +0900

    分子表示完全成功。15000_128

--- Analysis/plot_latents.py
+++ Analysis/plot_latents.py
@@ -1,3 +1,5 @@
+from pickletools import markobject
+
 import numpy as np
 import umap
 from sklearn.manifold import TSNE
@@ -6,11 +8,14 @@ import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.inf)
 
 # DATA_PATH = "/Users/taka/Documents/final_infer_on_all_data/40000_16/"
-DATA_PATH = "/Users/taka/Downloads/original/"
+DATA_PATH = "/"
 DIMENSION = 16
+N_NEIGHBORS = 2
+MIN_DIST = 0.01
+SPREAD = 1
 # BATCH_SIZE = 8000
-EPOCH_START = 2
-SAMPLE_LATENT = 30000
+EPOCH_START = 1
+SAMPLE_LATENT = 300000
 EPOCH_END = EPOCH_START + 1
 MODE = "umap"  # Choose between "tsne" and "umap"
 # MODE = "tsne"  # Choose between "tsne" and "umap"
@@ -74,90 +79,96 @@ def plot_tsne(cb_arr, latent_arr, epoch, perplexity, cb_size):
             plt.colorbar(label='Density')
             plt.show()
 
-def plot_umap(cb_arr, latent_arr, epoch, n_neighbors, min_dist, cb_size, zoom):
-    print("reducer setup")
-    reducer = umap.UMAP(
-        n_neighbors=n_neighbors,
-        min_dist=min_dist,
-        n_components=2,
-        n_epochs=5000,
-        random_state=42
-    ).fit(latent_arr)
-    print("reducer setup done")
-    latent_emb = reducer.transform(latent_arr)
-    print("latent transform done")
-    cb_emb = reducer.transform(cb_arr)
-    print("cb transform done")
-    x_range = np.percentile(cb_emb[:, 0], [50 - zoom, 50 + zoom])
-    y_range = np.percentile(cb_emb[:, 1], [50 - zoom, 50 + zoom])
+def plot_umap(cb_arr, latent_arr, epoch, n_neighbors, min_dist, cb_size, zoom, samples):
+    for SPREAD in [0.1, 0.2, 0.5]:
+        print("reducer setup")
+        reducer = umap.UMAP(
+            n_neighbors=N_NEIGHBORS,
+            min_dist=MIN_DIST,
+            spread=SPREAD,
+            n_components=2,
+            n_epochs=100,
+            random_state=42
+        ).fit(latent_arr)
+        print("reducer setup done")
+        latent_emb = reducer.transform(latent_arr)
+        print("latent transform done")
+        cb_emb = reducer.transform(cb_arr)
+        print("cb transform done")
+        x_range = np.percentile(cb_emb[:, 0], [50 - zoom, 50 + zoom])
+        y_range = np.percentile(cb_emb[:, 1], [50 - zoom, 50 + zoom])
 
-    latent_mask = (
-        (latent_emb[:, 0] >= x_range[0]) & (latent_emb[:, 0] <= x_range[1]) &
-        (latent_emb[:, 1] >= y_range[0]) & (latent_emb[:, 1] <= y_range[1])
-    )
-    cb_mask = (
-        (cb_emb[:, 0] >= x_range[0]) & (cb_emb[:, 0] <= x_range[1]) &
-        (cb_emb[:, 1] >= y_range[0]) & (cb_emb[:, 1] <= y_range[1])
-    )
+        latent_mask = (
+            (latent_emb[:, 0] >= x_range[0]) & (latent_emb[:, 0] <= x_range[1]) &
+            (latent_emb[:, 1] >= y_range[0]) & (latent_emb[:, 1] <= y_range[1])
+        )
+        cb_mask = (
+            (cb_emb[:, 0] >= x_range[0]) & (cb_emb[:, 0] <= x_range[1]) &
+            (cb_emb[:, 1] >= y_range[0]) & (cb_emb[:, 1] <= y_range[1])
+        )
 
-    zoomed_latent = latent_emb[latent_mask]
-    zoomed_cb = cb_emb[cb_mask]
+        zoomed_latent = latent_emb[latent_mask]
+        zoomed_cb = cb_emb[cb_mask]
 
-    bins = 200
-    title = f"UMAP: n_neighbors {n_neighbors}, min_dist {min_dist}, \n zoom {zoom} epoch {epoch}"
+        bins = 200
+        title = f"UMAP: n_neighbors {N_NEIGHBORS}, min_dist {MIN_DIST}, \n spread {SPREAD}, zoom {zoom} samples {samples}"
 
-    for i in range(2):
-        plt.figure()
-        plt.scatter(zoomed_latent[:, 0], zoomed_latent[:, 1], s=4, c='black')
-        # plt.hist2d(
-        #     zoomed_latent[:, 0], zoomed_latent[:, 1],
-        #     bins=[np.linspace(*x_range, bins), np.linspace(*y_range, bins)],
-        #     cmap="Blues"
-        # )
-        plt.xlim(x_range)
-        plt.ylim(y_range)
-        # plt.xlim(-30, 30)
-        # plt.ylim(-30, 30)
+        for i in range(2):
+            plt.figure()
+            if i == 0:
+                plt.scatter(zoomed_cb[:, 0], zoomed_cb[:, 1], s=20, c='red', alpha=0.2, marker='x')
+            plt.scatter(zoomed_latent[:, 0], zoomed_latent[:, 1], s=3, c='black')
+            # plt.hist2d(
+            #     zoomed_latent[:, 0], zoomed_latent[:, 1],
+            #     bins=[np.linspace(*x_range, bins), np.linspace(*y_range, bins)],
+            #     cmap="Blues"
+            # )
+            plt.xlim(x_range)
+            plt.ylim(y_range)
+            # plt.xlim(-30, 30)
+            # plt.ylim(-30, 30)
 
-        if i == 0:
-            plt.scatter(zoomed_cb[:, 0], zoomed_cb[:, 1], s=2, c='red', alpha=0.6)
-        plt.title(title + " (Zoomed)")
-        plt.colorbar(label='Density')
-        plt.show()
+            plt.title(title + " (Zoomed)")
+            plt.colorbar(label='Density')
+            plt.show()
 
 
-def process_epoch(epoch):
+def process_epoch(epoch, samples):
     """Load data and plot visualization for a single epoch."""
     codebook_file = f"{DATA_PATH}used_cb_vectors.npz"
     # codebook_file = "/Users/taka/PycharmProjects/vqatom/Analysis/kmeans_centers.npy"
-    codebook_file = f'{DATA_PATH}init_codebook_{epoch}.npz'
+    # codebook_file = f'{DATA_PATH}init_codebook_{epoch}.npz'
     # latent_file = f"{DATA_PATH}latents_all_{epoch}.npz"
-    latent_file = f"{DATA_PATH}latents_{epoch}.npz"
+    latent_file = f"{DATA_PATH}latents_all_{epoch}.npz"
 
     cb_arr = load_npz_array(codebook_file)
     latent_arr = load_npz_array_multi(latent_file)
     print("latent_arr.shape")
     print(latent_arr.shape)
-    latent_arr = latent_arr[:SAMPLE_LATENT]
+    latent_arr = latent_arr[:samples]
     print("latent_arr.shape")
     print(latent_arr.shape)
+    print("cb_arr.shape")
     print(cb_arr.shape)
 
     cb_arr = np.unique(cb_arr, axis=0).reshape(-1, DIMENSION)
     cb_size = cb_arr.shape[0]
+    print("cb_arr.shape")
+    print(cb_arr.shape)
 
     if MODE == "tsne":
         plot_tsne(cb_arr, latent_arr, epoch, perplexity=10, cb_size=cb_size)
     elif MODE == "umap":
-        for zoom in [50, 20, 15, 10, 7, 5, 3, 2]:
-        # for zoom in [50]:
-            plot_umap(cb_arr, latent_arr, epoch, n_neighbors=10, min_dist=1.0, cb_size=cb_size, zoom=zoom)
+        # for zoom in [50, 20, 15, 10, 7, 5, 3, 2]:
+        for zoom in [10]:
+            plot_umap(cb_arr, latent_arr, epoch, n_neighbors=10, min_dist=1.0, cb_size=cb_size, zoom=zoom, samples=samples)
 
 
 def main():
-    for epoch in range(EPOCH_START, EPOCH_END):
-        print(f"Processing epoch {epoch}")
-        process_epoch(epoch)
+    for samples in [30000, 300000, 3000000]:
+        for epoch in range(EPOCH_START, EPOCH_END):
+            print(f"Processing epoch {epoch}")
+            process_epoch(epoch, samples)
 
 
 if __name__ == '__main__':
