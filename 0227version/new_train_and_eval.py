9201e79f2588f18cf3086090297131fe4bcca365
taka <d@wakou.cc>
2025-06-18 16:55:06 +0900

    あああ

--- 0227version/new_train_and_eval.py
+++ 0227version/new_train_and_eval.py
@@ -325,6 +325,8 @@ def run_inductive(
             for idx, (adj_batch, attr_batch) in enumerate(dataloader):
                 if idx == 5:
                     break
+                if idx == 1:
+                    break
                 print(f"idx {idx}")
                 glist_base, glist = convert_to_dgl(adj_batch, attr_batch)  # 10000 molecules per glist
                 chunk_size = conf["chunk_size"]  # in 10,000 molecules
