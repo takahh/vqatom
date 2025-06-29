87886177140d2bfb28089e19d74a481e79e00e3f
taka <d@wakou.cc>
2025-06-29 09:07:08 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -173,7 +173,7 @@ def kmeans(
             dists = 1 - torch.matmul(samples_normalized, means_normalized.transpose(-1, -2))  # [H, N, k]
         else:
             with torch.cuda.amp.autocast(enabled=True):
-                dists = compute_chunked_dists_fast(samples, means[:, :, :k], chunk_size=500)
+                dists = compute_chunked_dists_fast(samples, means[:, :, :k], chunk_size=250)
 
         # Compute sampling probabilities
         min_dists = dists.min(dim=-1).values  # [H, N]
