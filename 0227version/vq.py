3651633b01530f8eee450331d2572e0ef187e9f4
taka <d@wakou.cc>
2025-07-03 15:06:06 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -340,8 +340,9 @@ class ContrastiveLoss(nn.Module):
         latent_repel_loss, sim_mat = calc_repel_loss(latent_similarity_matrix)
         attract_loss, sim_mat = attract_high_sim(sim_mat)
         attract_weight = 0.5  # 0.005
+        repel_weight = 0.5  # 0.005
 
-        final_loss = latent_repel_loss + attract_weight * attract_loss
+        final_loss = repel_weight * latent_repel_loss + attract_weight * attract_loss
         neg_loss = 1
 
         return final_loss, neg_loss, latent_repel_loss, latent_repel_loss
