a9cd764a804c28b48d329e5707d7f4b2abfb4fd1
taka <d@wakou.cc>
2025-07-07 09:10:38 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -306,9 +306,8 @@ class ContrastiveLoss(nn.Module):
 
         latent_repel_loss = calc_repel_loss(latent_dist_matrix, dynamic_threshold)
         attract_loss = calc_attractive_loss(latent_dist_matrix, dynamic_threshold)
-        print(f"latent_repel_loss {latent_repel_loss}, attract_loss {attract_loss}")
         attract_weight = 0.1  # 0.005
-        repel_weight = 100  # 0.005
+        repel_weight = 0.1  # 0.005
         print("repel_weight * latent_repel_loss + attract_weight * attract_loss")
         print(f"{repel_weight * latent_repel_loss}, {attract_weight * attract_loss}")
         final_loss = repel_weight * latent_repel_loss + attract_weight * attract_loss
