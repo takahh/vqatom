4df4321fa32a5db18e9e4ec95a7f304edd278e10
taka <d@wakou.cc>
2025-07-10 08:25:54 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -221,7 +221,8 @@ class ContrastiveLoss(nn.Module):
         def calc_repel_loss(dmat, sigma=3, threshold=1):
             attract_mask = dmat < threshold
             repel_mask = ~attract_mask
-            repel_loss = torch.exp(-dmat[repel_mask] ** 2 / (2 * sigma ** 2)).mean()
+            # repel_loss = torch.exp(-dmat[repel_mask] ** 2 / (2 * sigma ** 2)).mean()
+            repel_loss = torch.exp(-dmat ** 2 / (2 * sigma ** 2)).mean()
             return repel_loss
 
         def calc_attractive_loss(dmat, sigma=1, threshold=1):
