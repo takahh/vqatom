4ffcee6b580305b86a14892ccadf2e093d6a1829
taka <d@wakou.cc>
2025-07-16 12:02:34 +0900

    あああ

--- 0227version/new_train_and_eval.py
+++ 0227version/new_train_and_eval.py
@@ -330,7 +330,7 @@ def run_inductive(
                         # random_indices = np.random.choice(latent_train.shape[0], 20000, replace=False)
                         np.savez(f"./latents_{epoch}", latents.cpu().detach().numpy())
                     loss_list_list_train = [x + [y] for x, y in zip(loss_list_list_train, loss_list_train)]
-                    del latents
+                    del latents, loss, loss_list_train, latent_train, latents, cb_num_unique
                     gc.collect()
                     torch.cuda.empty_cache()
 
