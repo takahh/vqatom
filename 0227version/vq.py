ebf458d5a3a65c0449da0181a25c815eff7a375e
takahh <d@wakou.cc>
2025-08-01 11:58:47 +0900

    q

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -816,9 +816,9 @@ class VectorQuantize(nn.Module):
         EPOCH_TO_SHIFT = 2
         # ----------------------------------
 
-        # ------------DELETE THIS SOON !!!!!!!!! ----------------------
-        commit_loss, codebook_loss = self.commitment_loss(x.squeeze(), codebook)
-        # ------------DELETE THIS SOON !!!!!!!!! ----------------------
+        # # ------------DELETE THIS SOON !!!!!!!!! ----------------------
+        # commit_loss, codebook_loss = self.commitment_loss(x.squeeze(), codebook)
+        # # ------------DELETE THIS SOON !!!!!!!!! ----------------------
         if epoch >= EPOCH_TO_SHIFT:
             commit_loss, codebook_loss = self.commitment_loss(x.squeeze(), codebook)
         else:
