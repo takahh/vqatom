b67d35030238d2cc626483d2fc42e24f54ee0960
taka <d@wakou.cc>
2025-10-22 20:41:48 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -1517,7 +1517,7 @@ class VectorQuantize(nn.Module):
             ret = self.compute_contrastive_loss(z, 0, logger, codebook[str(key)])
             repel_value = ret[0]
             cb_repel_value = ret[3]
-            print(f"repel_value {repel_value}, cb_rep_value {cb_repel_value}")
+            print(f"{key} : commit {commit_part}, repel {repel_value}, cb_repel {cb_repel_value}")
             repel_num = repel_num + repel_value * Ni
             cb_repel_num = cb_repel_num + cb_repel_value * Ni
 
