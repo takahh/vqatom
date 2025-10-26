268fd7b2f8ed416a411c0323a2d60ad6da3e049b
taka <d@wakou.cc>
2025-10-26 16:50:36 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -1736,10 +1736,10 @@ class VectorQuantize(nn.Module):
         # elif epoch >= 3:
         #     print(f"repel {repel_loss}") # or some decaying schedule
         # Assuming commitment_loss returns raw components:
-        beta_commit = 1000  # try 0.25–0.5 if you see codebook collapse
-        gamma_cb = 300  # codebook (EMA) regularizer
+        beta_commit = 100  # try 0.25–0.5 if you see codebook collapse
+        gamma_cb = 30  # codebook (EMA) regularizer
         delta_mid = 0.1  # repel (midpoints)
-        delta_cb = 0.03  # codebook-codebook repel
+        delta_cb = 0.0003  # codebook-codebook repel
         # commit_loss 2.4284323444589972e-05  >> 2e-02
         # codebook_loss 9.713729377835989e-05
         # repel_loss 0.32586005330085754
