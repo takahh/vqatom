6cdebfdf02d307f1b3666d77927a8df29d5c35ca
taka <d@wakou.cc>
2025-10-30 10:25:58 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -217,7 +217,8 @@ def kmeans(
 
         # 3) Iteratively sample new centers
         for k in range(1, K):
-            print(f"{k},", end="")
+            if k % 100 == 0:
+                print(f"{k},", end="")
             # Choose next center proportional to distance (optionally squared)
             probs = closest + eps
             if square_prob:
