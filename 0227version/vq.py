6371694d4e19dadb46bf363d4da67baca78cee59
taka <d@wakou.cc>
2025-11-13 08:47:04 +0900

    あああ

--- 0227version/vq.py
+++ 0227version/vq.py
@@ -2066,7 +2066,7 @@ class VectorQuantize(nn.Module):
         warmup = 5
         alpha = float(torch.exp(torch.tensor(-(epoch - warmup) / 50.0)))
         # if epoch < warmup:
-        loss = (beta_commit * commit_loss) + (gamma_cb * codebook_loss)
+        loss = beta_commit * commit_loss
         # loss = (alpha * repel_loss) + (beta_commit * commit_loss) + (gamma_cb * codebook_loss)
         # else:
         #     # half-life ~ 50 epochs
