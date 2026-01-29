6edbae2509bdbd6c97154f7e40ab025ceca1833f
taka <d@wakou.cc>
2026-01-30 08:41:26 +0900

    あああ

--- 0227version/train_teacher.py
+++ 0227version/train_teacher.py
@@ -285,7 +285,7 @@ def run(args):
             logger
         )
     elif args.train_or_infer == "infer":
-        run_infer_only_after_restore(conf, model, logger, "model_epoch_10.pth")
+        run_infer_only_after_restore(conf, model, logger, "data/model_epoch_10.pth")
 
 def repeat_run(args):
     scores = []
