b5c573f734389caff793ea9854f3f623b688c7e4
taka <d@wakou.cc>
2026-03-08 20:25:58 +0900

    AUC 0.8026

--- dti/ablation.py
+++ dti/ablation.py
@@ -464,23 +464,22 @@ class QKOnlyDTIClassifier(nn.Module):
         self.q_proj = nn.Linear(d_model, d_model, bias=False)
         self.k_proj = nn.Linear(d_model, d_model, bias=False)
 
-        # self.head = nn.Sequential(
-        #     nn.LayerNorm(2 * d_model),
-        #     nn.Linear(2 * d_model, d_model),
-        #     nn.GELU(),
-        #     nn.Dropout(dropout),
-        #     nn.Linear(d_model, 1),
-        # )
-        self.head = nn.Linear(2 * d_model, 1)
-        # self.reg_head = nn.Sequential(
-        #     nn.LayerNorm(2 * d_model),
-        #     nn.Linear(2 * d_model, d_model),
-        #     nn.GELU(),
-        #     nn.Dropout(dropout),
-        #     nn.Linear(d_model, 1),
-        # )
-
-        self.reg_head = nn.Linear(2 * d_model, 1)
+        self.head = nn.Sequential(
+            nn.LayerNorm(2 * d_model),
+            nn.Linear(2 * d_model, d_model),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(d_model, 1),
+        )
+        # self.head = nn.Linear(2 * d_model, 1)
+        self.reg_head = nn.Sequential(
+            nn.LayerNorm(2 * d_model),
+            nn.Linear(2 * d_model, d_model),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(d_model, 1),
+        )
+        # self.reg_head = nn.Linear(2 * d_model, 1)
         self.lig_pad_id = int(self.lig.pad_id)
 
     def forward(self, p_input_ids, p_attn_mask, l_ids):
