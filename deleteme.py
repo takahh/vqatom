ef074a26b1efec61e6948ce0ff03450df264480c
Anonymous <your_new_email@gmail.com>
2026-05-21 01:24:47 +0900

    undone wrong file move

--- deleteme.py
+++ deleteme.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # =========================================================
 # Config
 # =========================================================
-json_dir = "/Users/taka/Downloads/json"
+json_dir = "/Users/taka/Downloads/json_gnn"
 files = glob.glob(os.path.join(json_dir, "epoch_*.json"))
 
 
